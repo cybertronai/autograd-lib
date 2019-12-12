fix packaging issue, add LeastSquares hessian + test

autograd_lib/autograd_lib.py
@@ -104,16 +104,30 @@ def backward_jacobian(output: torch.Tensor, retain_graph=False) -> None:
 
 
 def backward_hessian(output, loss='CrossEntropy', retain_graph=False) -> None:
-    assert loss in ('CrossEntropy',), f"Only CrossEntropy loss is supported, got {loss}"
+    supported_losses =  ('CrossEntropy', 'LeastSquares')
+    assert loss in supported_losses, f"Only {supported_losses} loss is supported, got {loss}"
+
     assert u.is_matrix(output)
+    
+    if loss == 'CrossEntropy':
+        # use Cholesky-like decomposition from https://www.wolframcloud.com/obj/yaroslavvb/newton/square-root-formulas.nb
+        n, o = output.shape
+        p = F.softmax(output, dim=1)
 
-    # use Cholesky-like decomposition from https://www.wolframcloud.com/obj/yaroslavvb/newton/square-root-formulas.nb
-    n, o = output.shape
-    p = F.softmax(output, dim=1)
+        mask = torch.eye(o).to(output.device).expand(n, o, o)
+        diag_part = p.sqrt().unsqueeze(2).expand(n, o, o) * mask
+        hess_sqrt = diag_part - torch.einsum('ij,ik->ijk', p.sqrt(), p)   # n, o, o
 
-    mask = torch.eye(o).to(output.device).expand(n, o, o)
-    diag_part = p.sqrt().unsqueeze(2).expand(n, o, o) * mask
-    hess_sqrt = diag_part - torch.einsum('ij,ik->ijk', p.sqrt(), p)   # n, o, o
+        for out_idx in range(o):
+            output.backward(hess_sqrt[:, out_idx, :], retain_graph=(retain_graph or out_idx < o - 1))
 
-    for out_idx in range(o):
-        output.backward(hess_sqrt[:, out_idx, :], retain_graph=(retain_graph or out_idx < o - 1))
+    elif loss == 'LeastSquares':
+        n, o = output.shape
+        hess = []
+
+        id_mat = torch.eye(o)
+        for idx in range(o):
+            hess.append(torch.stack([id_mat[idx]] * n))
+
+        for idx in range(o):
+            output.backward(hess[idx], retain_graph=(retain_graph or idx < o - 1))

autograd_lib_test.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
 
 import torch
+import sys
 from attrdict import AttrDefault
 
-import autograd_lib
-import util as u
+from autograd_lib import autograd_lib
+from autograd_lib import util as u
 
 
 def create_toy_model():
@@ -159,6 +160,55 @@ def test_hessian_diag():
             u.check_close(diag_autograd, hess_diag[layer]/batch_size)
 
 
+def test_hessian_diag_sqr():
+    """Like above, but using LeastSquares loss"""
+    
+    data_width = 3
+    batch_size = 2
+    d = [data_width**2, 6, 10]
+    train_steps = 2
+
+    model: u.SimpleMLP = u.SimpleMLP(d, nonlin=True, bias=True)
+    autograd_lib.register(model)
+    dataset = u.TinyMNIST(dataset_size=batch_size*train_steps, data_width=data_width)
+    trainloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    train_iter = iter(trainloader)
+
+    #loss_fn = torch.nn.CrossEntropyLoss()
+    loss_fn = u.least_squares
+
+    for train_step in range(train_steps):
+        data, targets = next(train_iter)
+
+        hess = defaultdict(float)
+        hess_diag = defaultdict(float)
+        activations = {}
+        def save_activations(layer, a, _):
+            activations[layer] = a
+
+        with autograd_lib.module_hook(save_activations):
+            output = model(data)
+            loss = loss_fn(output, torch.zeros_like(output))
+
+        def compute_hess(layer, _, B):
+            A = activations[layer]
+            BA = torch.einsum("nl,ni->nli", B, A)
+            hess[layer] += torch.einsum('nli,nkj->likj', BA, BA)
+            hess_diag[layer] += torch.einsum("ni,nj->ij", B * B, A * A)
+
+        with autograd_lib.module_hook(compute_hess):
+            autograd_lib.backward_hessian(output, loss='LeastSquares', retain_graph=True)
+
+        # compute Hessian through autograd
+        # for layer in model.layers:
+
+        for layer in model.layers:
+            H_autograd = u.hessian(loss, layer.weight)
+            u.check_close(hess[layer] / batch_size, H_autograd)
+            diag_autograd = torch.einsum('lili->li', H_autograd)
+            u.check_close(diag_autograd, hess_diag[layer]/batch_size)
+
+
 def test_jacobian_full():
     """Test computing Jacobian squared"""
     A, model = create_toy_model()
@@ -234,6 +284,8 @@ def test_fisher_full():
 
 
 if __name__ == '__main__':
+    test_hessian_diag_sqr()
+    # sys.exit()
     test_jacobian_full()
     test_fisher_full()
     test_hessian_full()

example_hessian.py
@@ -27,7 +27,7 @@ d=1
 n=1
 model = simple_model(1, 5)
 data = torch.ones((n, d))
-targets = torch.zeros((n, d))
+targets = torch.ones((n, d))
 loss_fn = least_squares
 
 autograd_lib.register(model)
@@ -62,7 +62,7 @@ def compute_hess(layer, _, B):
 
 
 with autograd_lib.module_hook(compute_hess):
-    autograd_lib.backward_hessian(output, loss='CrossEntropy')
+    autograd_lib.backward_hessian(output, loss='LeastSquares')
 
 for layer in model.modules():
     print(hess_diag[layer])